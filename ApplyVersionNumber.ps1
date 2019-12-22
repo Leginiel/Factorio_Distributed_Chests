7176f45c7ea728fccedb183ce83b7f3f6b4b23b8
Sebastian Berning <hunter.Vasitas@googlemail.com>
2019-12-22 16:18:26 +0100

    changed version ps1

--- ApplyVersionNumber.ps1
+++ ApplyVersionNumber.ps1
@@ -1,21 +1,27 @@
 $VersionRegexWithoutMinor = "\d+\.\d+\."
 
-foreach { gci -Path $_.FullName -Recurse -include *.json }
+$files = gci $Env:BUILD_SOURCESDIRECTORY -recurse | 
+		?{ $_.PSIsContainer } | 
+		foreach { gci -Path $_.FullName -Recurse -include *.csproj }
 if($files)
 {
-  foreach ($file in $files) {
-    $filecontent = Get-Content($file)
-    $VersionData = [regex]::matches($filecontent,$VersionRegexWithoutRevision)
-    $NewVersion = $VersionData[0]
-    Write-Verbose "VersionData: $NewVersion"
-    Write-Verbose "VersionDataxx: $Env:BUILD_BUILDNUMBER"
-    $NewVersion = "$NewVersion$Env:BUILD_BUILDNUMBER"
-    Write-Verbose "Version: $NewVersion"
+		foreach ($file in $files) {
+				$filecontent = Get-Content($file)
+				$VersionData = [regex]::matches($filecontent,$VersionRegexWithoutRevision)
+				$NewVersion = $VersionData[0]
+				Write-Verbose "VersionData: $NewVersion"
+				Write-Verbose "VersionDataxx: $Env:BUILD_BUILDNUMBER"
+				$NewVersion = "$NewVersion$Env:BUILD_BUILDNUMBER"
+				Write-Verbose "Version: $NewVersion"
 
-    attrib $file -r
-    $filecontent -replace $VersionRegex, $NewVersion | Out-File $file
-    Write-Verbose "$file.FullName - version applied"
-  }
+				attrib $file -r
+				$filecontent -replace $VersionRegex, $NewVersion | Out-File $file
+				Write-Verbose "$file.FullName - version applied"
+		}
 
-  $Env:BUILD_BUILDNUMBER = $NewVersion
+		$Env:BUILD_BUILDNUMBER = $NewVersion
+}
+else
+{
+		Write-Warning "Found no files."
 }
